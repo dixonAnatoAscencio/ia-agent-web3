e7f8ae87120110af6da0348f4b9efae05a9a6e91
DixonCi2 <dixon.anato@ci2.co>
2024-12-16 19:04:03 -0500

    add: funciones del modelo y crear asistente, crear thread

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
+import OpenAi from "openai"
+import "dotenv/config"
+import { createAssistant } from "./openai/createAssistant";
+import { createThread } from "./openai/createThread";
+import { createRun } from "./openai/createRun";
+import { performRun } from "./openai/performRun";
+
 async function main() {
+    const client = new OpenAi()
+    const message = "Hello, Alt Cunningham!"
+
+    const assistant = await createAssistant(client)
+    const thread = await createThread(client, message)
+    const run = await createRun(client, thread, assistant.id)
+    const result = await performRun(run, client, thread)
+
+
+
+
+
     console.log("Hello World!");
 }
 
